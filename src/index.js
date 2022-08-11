c67a384ecbc77ab85dc2c1ab65cd14f10d477763
Ihar Sapeha <maddavoice@gmail.com>
2022-08-11 20:50:28 +0300

    feat: add func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    return false;
+  } 
+
+  let arr = [];
+
+  for (let i = 0; i < str.length; i++) {
+
+    let activeBracket = str[i];
+
+    for (let a = 0; a < bracketsConfig.length; a++) {
+
+      let element = arr[arr.length - 1];
+
+      if (bracketsConfig[a][1] == activeBracket && bracketsConfig[a][0] == element) {
+        arr.pop();
+      } else if (bracketsConfig[a][0] == activeBracket) {
+        arr.push(activeBracket);
+      }
+    }
+  }
+
+  if (arr.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
